1e2348a2229650113e33952a94f6fac901ee6440
Jelly <Tieukieutien2000@gmail.com>
2022-08-22 10:21:48 +0700

    add wrong trading view

--- src/pages/DetailsCoinPage.jsx
+++ src/pages/DetailsCoinPage.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-undef */
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import { useNavigate, useParams } from "react-router-dom";
 import InfoContent from "../components/detailPage/InfoContent";
 import { methodCall } from "../utils/request";
@@ -16,47 +18,39 @@ export default function DetailsCoinPage() {
       params: [symbol_id],
     });
     StaticStore.SymbolInfo[symbol_id] = data.result;
-    console.log("lay data coin",StaticStore.SymbolInfo);
+    console.log("lay data coin", StaticStore.SymbolInfo);
     console.log("gan du lieu cho coin");
-    console.log(StaticStore.SymbolInfo)
+    console.log(StaticStore.SymbolInfo);
     for (var i in StaticStore.SymbolInfo) {
-      console.log(i)
+      console.log(i);
       if (i === id) {
-        console.log(i)
-        setCoin(StaticStore.SymbolInfo[i])
-      }
-      else{
-        console.log('false')
+        console.log(i);
+        setCoin(StaticStore.SymbolInfo[i]);
+      } else {
+        console.log("false");
       }
     }
   };
-
- 
 
   useEffect(() => {
     var isEmptyObj = Object.keys(StaticStore.SymbolInfo).length;
     if (isEmptyObj) {
       console.log("da co data");
       for (var i in StaticStore.SymbolInfo) {
-        console.log(i)
+        console.log(i);
         if (i === id) {
-          console.log(i)
-          setCoin(StaticStore.SymbolInfo[i])
-        }
-        else{
-          console.log('false')
+          console.log(i);
+          setCoin(StaticStore.SymbolInfo[i]);
+        } else {
+          console.log("false");
         }
       }
-
     } else {
       console.log("chua co data");
-      listSymbolInfo.forEach((symbol_id) =>  asyncGetDataCoin(symbol_id)
-      )
-
+      listSymbolInfo.forEach((symbol_id) => asyncGetDataCoin(symbol_id));
     }
     console.log(StaticStore);
   }, []);
-  
   return (
     <div>
       <div>
@@ -84,42 +78,121 @@ export default function DetailsCoinPage() {
 
         {/* coin info */}
         <div className="my-4 border-solid border-2 border-red-400">
-
-        <InfoContent coin={coin} />
+          <InfoContent coin={coin} />
         </div>
         {/*Coin data and community */}
 
         <div>
-        <div>
-          <ul className="flex py-10 border-solid border-y border-slate-400">
-            <li className="px-5 mx-5 flex h-12 bg-blue-600 text-white rounded-xl content-center font-bold hover:bg-blue-700 items-center text-center">Overview</li>
-            <li className="px-5 mx-5 flex h-12 rounded-xl content-center font-bold hover:bg-slate-400 items-center text-center">Markets</li>
-            <li className="px-5 mx-5 flex h-12 rounded-xl content-center font-bold hover:bg-slate-400 items-center text-center">Historical Data</li>
-            <li className="px-5 mx-5 flex h-12 rounded-xl content-center font-bold hover:bg-slate-400 items-center text-center">News</li>
-            <li className="px-5 mx-5 flex h-12 rounded-xl content-center font-bold hover:bg-slate-400 items-center text-center">Price Estimates</li>
-            <li className="px-5 mx-5 flex h-12 rounded-xl content-center font-bold hover:bg-slate-400 items-center text-center">More Info</li>
-
-          </ul>
-        </div>
-        <div className="flex border-solid border-2 border-yellow-400">
-          <div className="w-2/3 border-solid border-2 border-indigo-600 mr-5">
-            <div>chart</div>
-            <div>Infor</div>
-            <div>social bitcoin price estimates</div>
+          <div>
+            <ul className="flex py-10 border-solid border-y border-slate-400">
+              <li className="px-5 mx-5 flex h-12 bg-blue-600 text-white rounded-xl content-center font-bold hover:bg-blue-700 items-center text-center">
+                Overview
+              </li>
+              <li className="px-5 mx-5 flex h-12 rounded-xl content-center font-bold hover:bg-slate-400 items-center text-center">
+                Markets
+              </li>
+              <li className="px-5 mx-5 flex h-12 rounded-xl content-center font-bold hover:bg-slate-400 items-center text-center">
+                Historical Data
+              </li>
+              <li className="px-5 mx-5 flex h-12 rounded-xl content-center font-bold hover:bg-slate-400 items-center text-center">
+                News
+              </li>
+              <li className="px-5 mx-5 flex h-12 rounded-xl content-center font-bold hover:bg-slate-400 items-center text-center">
+                Price Estimates
+              </li>
+              <li className="px-5 mx-5 flex h-12 rounded-xl content-center font-bold hover:bg-slate-400 items-center text-center">
+                More Info
+              </li>
+            </ul>
           </div>
-          <div className="w-1/3 border-solid border-2 border-indigo-600">
-            <div className="flex">
-              <div>live Bitcoin Chat</div>
-              <div>View on community</div>
+          <div className="flex border-solid border-2 border-yellow-400">
+            <div className="w-2/3 border-solid border-2 border-indigo-600 mr-5">
+              <div>
+    {/* <div className="tradingview-widget-container">
+      <div id="tradingview_0352e"></div>
+      
+      <div className="tradingview-widget-copyright"><a href="https://vn.tradingview.com/symbols/BTCUSDT/?exchange=BINANCE" rel="noopener" target="_blank"><span class="blue-text">BTCUSDT Biểu đồ</span> </a> bởi TradingView</div>
+            <Helmet>
+                <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+        <script type="text/javascript">
+              {new TradingView.widget(
+              {
+              "autosize": true,
+              "symbol": "BINANCE:BTCUSDT",
+              "interval": "D",
+              "timezone": "Etc/UTC",
+              "theme": "light",
+              "style": "1",
+              "locale": "vi_VN",
+              "toolbar_bg": "#f1f3f6",
+              "enable_publishing": false,
+              "allow_symbol_change": true,
+              "container_id": "tradingview_0352e"
+            }
+        )}
+        </script>
+            </Helmet>
+    </div> */}
+              </div>
+
+              <div className="border border-solid border-slate-700">
+                <div>
+                  <div>{coin.symbol} to USD Converter</div>
+                  <div className="border border-solid border-slate-200 rounded-2xl shadow-sm p-5 m-5 flex items-center">
+                    <div>{console.log(coin)}</div>
+                    <div className="w-1/2 flex">
+                      <img
+                        className="w-14 h-14"
+                        src={coin.logo}
+                        alt={coin.symbol}
+                      />
+                      <div>
+                        <div>{coin.symbol}</div>
+                        <div className="font-bold">{coin.name}</div>
+                      </div>
+                      <input
+                        className="w-full placeholder:text-right"
+                        placeholder="1"
+                      />
+                    </div>
+                    <div
+                      className="border border-solid border-slate-200 rounded-full w-12 h-12
+                      text-center flex items-center"
+                    >
+                      <i className="bi bi-arrow-left-right m-auto "></i>
+                    </div>
+                    <div className="flex justify-between">
+                      <div className=" flex">
+                        <i className="bi bi-currency-dollar bg-teal-400 w-12 h-12  rounded-full"></i>
+                        <div className="">
+                          <div>USD</div>
+                          <div>United States Dollar</div>
+                        </div>
+                      </div>
+                      <input
+                        className="w-full placeholder:text-right"
+                        placeholder="21000"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div></div>
+                <div></div>
+              </div>
+              <div>social bitcoin price estimates</div>
             </div>
-            <div>chat table</div>
-            <div>community rule</div>
-            <div>BTC Price Statistics</div>
-            <div>Trending Coin and Tokens</div>
-            <div></div>
+            <div className="w-1/3 border-solid border-2 border-indigo-600">
+              <div className="flex">
+                <div>live Bitcoin Chat</div>
+                <div>View on community</div>
+              </div>
+              <div>chat table</div>
+              <div>community rule</div>
+              <div>BTC Price Statistics</div>
+              <div>Trending Coin and Tokens</div>
+              <div></div>
+            </div>
           </div>
-
-        </div>
         </div>
 
         {/* coin's market*/}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { data } from "../utils/data";
@@ -7,46 +7,53 @@ import "react-d3-treemap/dist/react.d3.treemap.css";
 import { useEffect } from "react";
 import StaticStore from "../utils/StaticStore";
 import { eventList } from "../utils/constants/eventLists";
+import Input from "../components/login/Input";
 
 export default function Login() {
   const navigate = useNavigate();
-  const [dataAll, setDataAll] = useState(data);
-  const [temp,setTemp] = useState()
+  const listCoin = ["BTC", "ETH", "LPT", "BNB", "BSW", "ADA", "DOGE"];
+  const [dataAll, setDataAll] = useState(listCoin);
+  const usernameRef = useRef();
+  const PasswordRef = useRef();
 
-  const listCoin = ["BTC", "ETH", "LPT", "BNB", "BSW","ADA","DOGE"]
-  useEffect(()=>{
+  useEffect(() => {
     const listenData = StaticStore.appEvent.subscribe(async (msg) => {
       if (msg.type === eventList.UPDATE_MARKET_DATA) {
         const [exchangeSplit, tradeTypeSplit, currency1Split, currency2Split] =
           msg.symbol_id?.split("_");
-
-          listCoin.map((coin)=>{
-            console.log(coin)
-            if (currency1Split===coin.symbol) {
-              const dataRowTempt = {
-                ...StaticStore.StructureData[
-                  `BINANCE_SPOT_${currency1Split}_USDT`
-                ],
-                info: StaticStore.SymbolInfo[currency1Split]
-              };
-              console.log(dataRowTempt)
-              setTemp({ ...dataRowTempt });
-              
-            }
-          })
-        
+        // console.log(StaticStore.StructureData[msg.symbol_id])
+        listCoin.map((coin) => {
+          // console.log(coin)
+          if (currency1Split === coin) {
+            const dataRowTempt = {
+              name: StaticStore.SymbolInfo[currency1Split].name,
+              value:
+                StaticStore.StructureData[`BINANCE_SPOT_${currency1Split}_USDT`]
+                  .ohlcv["12HRS"].price_close,
+            };
+            // console.log(currency1Split)
+            // console.log(dataRowTempt);
+            setDataAll({ ...dataRowTempt });
+          }
+        });
       }
     });
-
     return () => {
       listenData.unsubscribe();
     };
-
   }, []);
-  useEffect(()=>{
-    console.log(temp)
-  },[temp])
+  useEffect(() => {
+    // console.log(dataAll);
+  }, [dataAll]);
 
+  console.log("render");
+  useEffect(() => {}, [usernameRef, PasswordRef]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = new FormData(e.target)
+    console.log(Object.fromEntries(user.entries()))
+    console.log(user);
+  };
 
   return (
     <div className="h-screen flex">
@@ -68,14 +75,15 @@ export default function Login() {
               Sign up
             </div>
           </div>
-          <div className="my-10">
+          <form onSubmit={handleSubmit} className="my-10">
             <div>
               <div className="text-slate-500">Email Address</div>
-              <input
-                className="border rounded-xl w-full h-20 placeholder:p-10"
-                type="email"
-                placeholder="Enter your email address..."
-              ></input>
+              <Input
+                name = 'username'
+                refer={usernameRef}
+                placeholder="Enter your email..."
+                type="text"
+              />
             </div>
 
             <div>
@@ -83,52 +91,48 @@ export default function Login() {
                 <div>Password</div>
                 <div className="cursor-pointer">Forgot password?</div>
               </div>
-              <input
-                className="border rounded-xl w-full h-20 placeholder:p-10"
-                type="password"
+              <Input
+                name = 'password'
+                refer={PasswordRef}
                 placeholder="Enter your password..."
-              ></input>
+                type="password"
+              />
             </div>
             <button className=" p-5 my-5 font-bold rounded-2xl border border-solid border-blue-700 w-full">
               Log in
             </button>
-            <div className="h-2 w-full border-t-2 relative"></div>
-            <button className=" p-5 my-5 font-bold rounded-2xl flex items-center w-full border border-solid border-yellow-400">
-              <div className="flex m-auto">
-                <svg
-                  className="w-8 h-8"
-                  viewBox="0 0 126.61 126.61"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g fill="#f3ba2f">
-                    <path d="m38.73 53.2 24.59-24.58 24.6 24.6 14.3-14.31-38.9-38.91-38.9 38.9z" />
-                    <path d="m0 63.31 14.3-14.31 14.31 14.31-14.31 14.3z" />
-                    <path d="m38.73 73.41 24.59 24.59 24.6-24.6 14.31 14.29-38.9 38.91-38.91-38.88z" />
-                    <path d="m98 63.31 14.3-14.31 14.31 14.3-14.31 14.32z" />
-                    <path d="m77.83 63.3-14.51-14.52-10.73 10.73-1.24 1.23-2.54 2.54 14.51 14.5 14.51-14.47z" />
-                  </g>
-                </svg>
-                Continue with Binance
-              </div>
-            </button>
-            <button className="p-5 font-bold rounded-2xl flex w-full items-center border border-solid border-blue-800">
-              <div className="m-auto">
-                <i class="bi bi-wallet-fill mx-2"></i>
-                Continue with wallet
-              </div>
-            </button>
-          </div>
+          </form>
+          <div className="h-2 w-full border-t-2 relative"></div>
+          <button className=" p-5 my-5 font-bold rounded-2xl flex items-center w-full border border-solid border-yellow-400">
+            <div className="flex m-auto">
+              <svg
+                className="w-8 h-8"
+                viewBox="0 0 126.61 126.61"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g fill="#f3ba2f">
+                  <path d="m38.73 53.2 24.59-24.58 24.6 24.6 14.3-14.31-38.9-38.91-38.9 38.9z" />
+                  <path d="m0 63.31 14.3-14.31 14.31 14.31-14.31 14.3z" />
+                  <path d="m38.73 73.41 24.59 24.59 24.6-24.6 14.31 14.29-38.9 38.91-38.91-38.88z" />
+                  <path d="m98 63.31 14.3-14.31 14.31 14.3-14.31 14.32z" />
+                  <path d="m77.83 63.3-14.51-14.52-10.73 10.73-1.24 1.23-2.54 2.54 14.51 14.5 14.51-14.47z" />
+                </g>
+              </svg>
+              Continue with Binance
+            </div>
+          </button>
+          <button className="p-5 font-bold rounded-2xl flex w-full items-center border border-solid border-blue-800">
+            <div className="m-auto">
+              <i class="bi bi-wallet-fill mx-2"></i>
+              Continue with wallet
+            </div>
+          </button>
         </div>
       </div>
       {/* Heat map  */}
       <div className="w-1/2 h-fit bg-white">
         <div className="m-auto">
-          <TreeMap
-            height={600}
-            width={600}
-            data={dataAll ? dataAll : data}
-            valueUnit={"Coin"}
-          />
+          <TreeMap height={600} width={600} data={dataAll} valueUnit={"Coin"} />
         </div>
       </div>
     </div>
